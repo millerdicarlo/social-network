caaaa6cc802286c845a743fc1f959b7113c642a6
millerd <millerdicarlo@gmail.com>
2022-10-06 19:51:55 -0400

    User.js updated

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,6 +3,10 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const thoughtRoutes = require('./thought-routes');
 
+// Import all of the API routes from /api/index.js (no need for index.js though since it's implied)
+const apiRoutes = require('./api');
+const htmlRoutes = require('./html/html-routes');
+
 // add prefix 
 router.use('/users', userRoutes);
 router.use('/thoughts', thoughtRoutes);
